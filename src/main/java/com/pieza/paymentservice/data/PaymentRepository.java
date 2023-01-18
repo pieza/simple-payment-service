2102f5d8c6a5c97663c85c9eb9bad222b675a08f
José Andrés Ulloa <julloah940@ulacit.ed.cr>
2023-01-17 17:13:04 -0800

    Initial Commit

---
+++ src/main/java/com/pieza/paymentservice/data/PaymentRepository.java
@@ -0,0 +1,8 @@
+package com.pieza.paymentservice.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository <PaymentEntity, String>{
+    
+}
+
